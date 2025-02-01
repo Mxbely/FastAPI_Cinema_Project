01cb83dd3a5b66cb51e5f160bf305af4418f66b8
Max <mxbely98@gmail.com>
2025-02-01 18:35:37 +0200

    implement profile schemas, endpoints, ad storaged and email sender, inluded routes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from routes import accounts_router, profiles_router
 
 app = FastAPI(
     title="Movies Cinema",
 )
 
 api_version_prefix = "/api/v1"
+
+app.include_router(accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
+app.include_router(profiles_router, prefix=f"{api_version_prefix}/profiles", tags=["profiles"])
