468be3c3e4b183567d01ff9c0d0fc0059cc2798f
Max <mxbely98@gmail.com>
2025-02-01 14:21:15 +0200

    check mypy settings

--- src/database/models/shoping_cart.py
+++ src/database/models/shoping_cart.py
@@ -3,7 +3,9 @@ from datetime import datetime
 from sqlalchemy import DateTime, ForeignKey, Integer, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from database import Base, Movie, User
+from database.models.base import Base
+from database.models.movies import Movie
+from database.models.accounts import User
 
 
 class Cart(Base):
