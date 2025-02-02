cf82fd53e31d3dea13963dbeaa2aa0eb5507f720
Den_k0 <denys.kotovskiy@gmail.com>
2025-02-03 01:19:30 +0200

    Include order routes to global app router

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from routes import accounts_router, profiles_router
+from routes import accounts_router, profiles_router, orders_router
 
 app = FastAPI(
     title="Movies Cinema",
@@ -13,4 +13,7 @@ app.include_router(
 )
 app.include_router(
     profiles_router, prefix=f"{api_version_prefix}/profiles", tags=["profiles"]
+)
+app.include_router(
+    orders_router, prefix=f"{api_version_prefix}", tags=["orders"]
 )
--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,2 +1,3 @@
 from routes.accounts import router as accounts_router
 from routes.profiles import router as profiles_router
+from routes.orders import router as orders_router
