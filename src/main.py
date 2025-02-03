5ba568670eb8df28c432395303303067378ba566
Den_k0 <denys.kotovskiy@gmail.com>
2025-02-04 00:06:56 +0200

    Include order routes to global app router

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 
-from routes import accounts_router, movies_router, payments_router, profiles_router
+from routes import accounts_router, movies_router, payments_router, profiles_router, orders_router
 from routes.shopping_cart import router as shopping_carts_router
 
 app = FastAPI(
@@ -21,6 +21,9 @@ app.include_router(
 )
 app.include_router(
     payments_router, prefix=f"{api_version_prefix}/payments", tags=["payments"]
+)
+app.include_router(
+    orders_router, prefix=f"{api_version_prefix}", tags=["orders"]
 )
 
 add_pagination(app)
