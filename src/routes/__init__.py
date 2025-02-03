23856453f267466da9f56797a7a9aa05226388ec
Bondzik-S <bondziks@gmail.com>
2025-02-03 23:27:01 +0200

    feat: import movies_router

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,3 +1,4 @@
 from routes.accounts import router as accounts_router
 from routes.payments import router as payments_router
 from routes.profiles import router as profiles_router
+from routes.movies import router as movies_router
