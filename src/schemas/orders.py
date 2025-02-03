f59330944f905f1c816f84fe63c4249c8eebaff1
Den_k0 <denys.kotovskiy@gmail.com>
2025-02-04 00:15:00 +0200

    Implement place_order route

--- src/schemas/orders.py
+++ src/schemas/orders.py
@@ -8,6 +8,24 @@ from database.models.orders import OrderStatusEnum
 """ Message... """
 class MessageResponseSchema(BaseModel):
     message: str
+#
+#
+# """ Cart schems (delete) """
+# class CartItemDetail(BaseModel):
+#     movie_id: int
+#     title: str
+#     price: float
+#     genre: str | None
+#     release_year: int
+#
+#
+# class CartResponse(BaseModel):
+#     user_id: int
+#     movies: list[CartItemDetail]
+#
+#
+# class PlaceOrderRequestSchema(BaseModel):
+#     ...
 
 
 """ 2. View User Orders """
@@ -29,3 +47,21 @@ class OrderItemResponseSchema(BaseModel):
     model_config = {
         "from_attributes": True
     }
+
+
+# class AllOrdersRequestSchema(BaseModel):
+#     user_id: int
+#
+#
+# class AllOrdersUserRequestSchema(AllOrdersRequestSchema):
+#     pass
+#
+#
+# class AllOrdersAdminRequestSchema(AllOrdersRequestSchema):
+#     date_from: datetime
+#     date_to: datetime
+#     status_filter: OrderStatusEnum
+#
+#     model_config = {
+#         "from_attributes": True
+#     }
