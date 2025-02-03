d9f2688a14672b14da3f45ca4594b82056b69c47
Bondzik-S <bondziks@gmail.com>
2025-02-03 23:25:38 +0200

    feat: include movie router to main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 
-from routes import accounts_router, payments_router, profiles_router
+from routes import accounts_router, movies_router, payments_router, profiles_router
 from routes.shopping_cart import router as shopping_carts_router
 
 app = FastAPI(
@@ -15,6 +15,9 @@ app.include_router(
 )
 app.include_router(
     profiles_router, prefix=f"{api_version_prefix}/profiles", tags=["profiles"]
+)
+app.include_router(
+    movies_router, prefix=f"{api_version_prefix}/cinema", tags=["cinema"]
 )
 app.include_router(
     payments_router, prefix=f"{api_version_prefix}/payments", tags=["payments"]
