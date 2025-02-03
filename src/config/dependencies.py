e3ee6e6e059f444e39639417cd98b2eabaf0b0dd
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2025-02-03 22:15:48 +0200

    Create email payment template and success payment email sending method

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -40,6 +40,7 @@ def get_accounts_email_notificator(
         activation_complete_email_template_name=settings.ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME,
         password_email_template_name=settings.PASSWORD_RESET_TEMPLATE_NAME,
         password_complete_email_template_name=settings.PASSWORD_RESET_COMPLETE_TEMPLATE_NAME,
+        payment_success_email_template_name=settings.PAYMENT_SUCCESS_TEMPLATE_NAME,
     )
 
 
