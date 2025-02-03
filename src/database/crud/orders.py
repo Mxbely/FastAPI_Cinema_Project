f48c62b241bb2af9f5c9a4bf2b5c84fe5b6bfe76
Den_k0 <denys.kotovskiy@gmail.com>
2025-02-04 00:30:34 +0200

    Separate routes and crud; add load_dotenv to setting;

---
+++ src/database/crud/orders.py
@@ -0,0 +1,101 @@
+from datetime import datetime
+
+from fastapi import HTTPException, status
+from sqlalchemy.orm import Session, joinedload
+
+from database import Order, OrderItem, Cart, User
+from schemas import OrderItemResponseSchema, MovieListItemSchema
+
+
+def create_order(user_id: int, db: Session) -> Order:
+    """Creates a new order for the current user."""
+    cart = db.query(Cart).filter(Cart.user_id == user_id).first()
+    if not cart or not cart.items:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Cart is empty."
+        )
+
+    order = Order(
+        user_id=user_id, total_amount=sum(item.movie.price for item in cart.items)
+    )
+    db.add(order)
+    db.commit()
+    db.refresh(order)
+
+    for cart_item in cart.items:
+        order_item = OrderItem(
+            order_id=order.id,
+            movie_id=cart_item.movie_id,
+            price_at_order=cart_item.movie.price,
+        )
+        db.add(order_item)
+        db.delete(cart_item)
+
+    db.commit()
+    return order
+
+
+def update_order_with_stripe_url(order: Order, stripe_url: str, db: Session) -> None:
+    """Updates the order with a Stripe URL."""
+    order.stripe_url = stripe_url
+    db.commit()
+
+
+def get_user_orders(
+    current_user: User,
+    db: Session,
+    user_id: int | None = None,
+    date_from: datetime | None = None,
+    date_to: datetime | None = None,
+    order_status: str | None = None,
+) -> list[OrderItemResponseSchema]:
+    """Retrieves orders for a specific user or all users for admin."""
+    if current_user.group.name != "user":
+        filters = []
+        if user_id:
+            filters.append(Order.user_id == user_id)
+        if date_from:
+            filters.append(Order.created_at >= date_from)
+        if date_to:
+            filters.append(Order.created_at <= date_to)
+        if order_status:
+            filters.append(Order.status == order_status)
+        orders_query = db.query(Order).filter(*filters)
+    else:
+        if user_id or date_from or date_to or order_status:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You don't have permission.",
+            )
+        orders_query = db.query(Order).filter(Order.user_id == current_user.id)
+
+    orders = orders_query.options(
+        joinedload(Order.order_items).joinedload(OrderItem.movie)
+    )
+    orders = orders.order_by(Order.created_at.desc()).all()
+
+    if not orders:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="No orders found."
+        )
+
+    order_list = [
+        OrderItemResponseSchema(
+            created_at=order.created_at,
+            movies=[
+                MovieListItemSchema(
+                    id=item.movie.id,
+                    name=item.movie.name,
+                    year=item.movie.year,
+                    time=item.movie.time,
+                    description=item.movie.description,
+                )
+                for item in order.order_items
+            ],
+            total_amount=order.total_amount,
+            status=order.status,
+        )
+        for order in orders
+    ]
+
+    return order_list
--- src/routes/orders.py
+++ src/routes/orders.py
@@ -1,20 +1,21 @@
 from datetime import datetime
 
-from fastapi import APIRouter, Depends, HTTPException, status, Query
-from sqlalchemy.orm import Session, joinedload
+from fastapi import APIRouter, Request, Depends, status, Query
+from sqlalchemy.orm import Session
 
 from database import get_db
+from database.crud.orders import (
+    create_order,
+    update_order_with_stripe_url,
+    get_user_orders,
+)
 from database.models.accounts import User
-from database.models.orders import Order, OrderItem, OrderStatusEnum
+from database.models.orders import OrderStatusEnum
 from routes.profiles import get_current_user
-from schemas.orders import OrderItemResponseSchema, MovieItemResponse, MessageResponseSchema
+from schemas.orders import OrderItemResponseSchema, MessageResponseSchema
+from services import create_checkout_session
 
 router = APIRouter()
-
-
-@router.get("/")
-def root() -> dict:
-    return {"message": "Hello Test"}
 
 
 """
@@ -22,9 +23,24 @@ def root() -> dict:
 Endpoint: POST /orders/
 Description: Allows users to place an order for movies in their cart.
 """
-# @router.post("/orders/")
-# def place_order(user_id: int, db: Session = Depends(get_db)):
-#     pass
+
+
+@router.post("/orders/")
+def place_order(
+    request: Request,
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+) -> MessageResponseSchema:
+    order = create_order(user_id=current_user.id, db=db)
+
+    stripe_url = create_checkout_session(
+        request=request, order=order, user_id=current_user.id, db=db
+    )
+    update_order_with_stripe_url(order, stripe_url, db=db)
+
+    return MessageResponseSchema(
+        message=f"Order placed successfully, your order_id: {order.id}"
+    )
 
 
 """
@@ -32,116 +48,54 @@ Description: Allows users to place an order for movies in their cart.
 Endpoint: GET /orders/
 Description: Retrieves a list of all orders placed by a specific user.
 """
-@router.get("/orders/", response_model=list[OrderItemResponseSchema], status_code=status.HTTP_200_OK)
-def get_user_orders(
+
+
+@router.get(
+    "/orders/",
+    response_model=list[OrderItemResponseSchema],
+    status_code=status.HTTP_200_OK,
+)
+def get_user_orders_route(
     user_id: int | None = Query(default=None, description="Filter orders by user ID"),
-    date_from: datetime | None = Query(default=None, description="Filter orders from this date"),
-    date_to: datetime | None = Query(default=None, description="Filter orders until this date"),
-    order_status: OrderStatusEnum | None = Query(default=None, description="Filter orders by status"),
+    date_from: datetime | None = Query(
+        default=None, description="Filter orders from this date"
+    ),
+    date_to: datetime | None = Query(
+        default=None, description="Filter orders until this date"
+    ),
+    order_status: OrderStatusEnum | None = Query(
+        default=None, description="Filter orders by status"
+    ),
     current_user: User = Depends(get_current_user),
-    db: Session = Depends(get_db)
-):
-    if current_user.group.name != "user":
-        filters = []
-        if user_id:
-            filters.append(Order.user_id == user_id)
-        if date_from:
-            filters.append(Order.created_at >= date_from)
-        if date_to:
-            filters.append(Order.created_at <= date_to)
-        if order_status:
-            filters.append(Order.status == order_status)
-        orders = db.query(Order).filter(*filters)
-    else:
-        if user_id or date_from or date_to or order_status:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="You don't have permission."
-            )
-        orders = db.query(Order).filter(Order.user_id == current_user.id)
-
-    orders = orders.options(joinedload(Order.order_items).joinedload(OrderItem.movie))
-    orders = orders.order_by(Order.created_at.desc()).all()
-
-    if not orders:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No orders found.")
-
-    order_list = [
-        OrderItemResponseSchema(
-            created_at=order.created_at,
-            movies=[                                # ToDo - change it in future (can be N+1)
-                MovieItemResponse(
-                    id=item.movie.id, name=item.movie.name
-                ) for item in order.order_items
-            ],
-            total_amount=order.total_amount,
-            status=order.status
-        )
-        for order in orders
-    ]
-
-    return order_list
-
-
-"""
-3. Confirm an Order
-Endpoint: POST /orders/{order_id}/confirm/
-Description: Confirms an order and redirects the user to a payment gateway.
-"""
-@router.post("/orders/{order_id}/confirm/", response_model=MessageResponseSchema, status_code=status.HTTP_200_OK)
-def confirm_order(order_id: int, db: Session = Depends(get_db)):
-    order = db.query(Order).filter(Order.id == order_id).first()
-    if not order:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found.")
-
-    if order.status != OrderStatusEnum.PENDING.value:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Only pending orders can be confirmed."
-        )
-
-    # ToDo - redirect to payment...
-
-    order.status = OrderStatusEnum.PAID.value
-    db.commit()
-    db.refresh(order)
-
-    return MessageResponseSchema(
-        message="Order successfully confirmed."
+    db: Session = Depends(get_db),
+) -> list[OrderItemResponseSchema]:
+    orders: list[OrderItemResponseSchema] = get_user_orders(
+        current_user=current_user,
+        db=db,
+        user_id=user_id,
+        date_from=date_from,
+        date_to=date_to,
+        order_status=order_status,
     )
+    return orders
 
 
 """
-4. Cancel an Order  
-Endpoint: POST /orders/{order_id}/cancel/
-Description: Allows users to cancel an order before payment is completed.
+3. Detail view of order
+Endpoint: GET /orders/{order_id}/
+Description: Allows user to view details about order.
 """
-@router.post("/orders/{order_id}/cancel/", response_model=MessageResponseSchema, status_code=status.HTTP_200_OK)
-def cancel_order(order_id: int, db: Session = Depends(get_db)):
-    order = db.query(Order).filter(Order.id == order_id).first()
-    if not order:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found.")
-
-    if order.status != OrderStatusEnum.PENDING.value:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Only pending orders can be canceled."
-        )
-
-    order.status = OrderStatusEnum.CANCELED.value
-    db.commit()
-    db.refresh(order)
-
-    return MessageResponseSchema(
-        message="Order successfully canceled."
-    )
 
 
 """
-5. Request a Refund
-Endpoint: POST /orders/{order_id}/refund_request/
-Description: Allows users to request a refund for a paid order.
+4. Update order  
+Endpoint: PATCH /orders/{order_id}/
+Description: Allows user to update order.
 """
-# @router.post("/orders/{order_id}/refund_request/", response_model=OrderRefundRequestSchema)
-# def request_refund(order_id: int, db: Session = Depends(get_db)):
-#     pass
+
+
+"""
+4. Delete order  
+Endpoint: DELETE /orders/{order_id}/
+Description: Allows user to delete order.
+"""
