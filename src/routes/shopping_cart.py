ec0aa1f0d24f63824f06b23442ed7e6510e8fc41
Andrey Dementyev <dementyev.andreyy@gmail.com>
2025-02-03 12:20:37 +0200

    updated routes to the main project code style & improved them according to the task

--- src/routes/shopping_cart.py
+++ src/routes/shopping_cart.py
@@ -1,13 +1,28 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import (
+    APIRouter,
+    Depends,
+    HTTPException,
+    status
+)
 from sqlalchemy.orm import Session
 
-from database import CartItem, Cart, User
+from config import get_jwt_auth_manager
+from database import (
+    CartItem,
+    User,
+    Order,
+    PaymentItem,
+    OrderItem,
+    Payment,
+    PaymentStatusEnum
+)
 from database.session_postgresql import get_postgresql_db
+from schemas.accounts import MessageResponseSchema
 from schemas.shopping_cart import (
     CartCreate,
     CartResponse,
     CartItemResponse,
-    CartItemDetail
+    PurchasedMoviesResponse
 )
 from database.crud.shopping_cart import (
     get_user_cart,
@@ -16,23 +31,41 @@ from database.crud.shopping_cart import (
     create_cart,
     add_cart_item,
     delete_cart_item,
-    get_purchased_movies
+    get_purchased_movies_from_db,
+    get_cart_items_details
 )
+from security.http import get_token
 from validation.shopping_cart import (
     validate_not_in_cart,
     validate_not_purchased,
     validate_movie_availability
 )
-from security import get_current_user
 
-router = APIRouter(prefix="/cart", tags=["Shopping Cart"])
+router = APIRouter()
 
 
 @router.get("/", response_model=CartResponse)
 def get_cart(
         db: Session = Depends(get_postgresql_db),
-        user: User = Depends(get_current_user)
+        token: str = Depends(get_token),
+        jwt_manager=Depends(get_jwt_auth_manager)
 ):
+    try:
+        payload = jwt_manager.decode_access_token(token)
+        user_id = payload.get("user_id")
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=str(e)
+        )
+
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
     cart = get_user_cart(user, db)
     if not cart:
         return CartResponse(user_id=user.id, movies=[])
@@ -43,8 +76,25 @@ def get_cart(
 def add_to_cart(
         cart_data: CartCreate,
         db: Session = Depends(get_postgresql_db),
-        user: User = Depends(get_current_user)
+        token: str = Depends(get_token),
+        jwt_manager=Depends(get_jwt_auth_manager)
 ):
+    try:
+        payload = jwt_manager.decode_access_token(token)
+        user_id = payload.get("user_id")
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=str(e)
+        )
+
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
     movie = get_movie_by_id(cart_data.movie_id, db)
     if not movie:
         raise HTTPException(status_code=404, detail="Movie not found")
@@ -64,8 +114,25 @@ def add_to_cart(
 def remove_from_cart(
         movie_id: int,
         db: Session = Depends(get_postgresql_db),
-        user: User = Depends(get_current_user)
+        token: str = Depends(get_token),
+        jwt_manager=Depends(get_jwt_auth_manager)
 ):
+    try:
+        payload = jwt_manager.decode_access_token(token)
+        user_id = payload.get("user_id")
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=str(e)
+        )
+
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
     cart = get_user_cart(user, db)
     if not cart:
         raise HTTPException(status_code=404, detail="Cart not found")
@@ -74,15 +141,32 @@ def remove_from_cart(
     if not cart_item:
         raise HTTPException(status_code=404, detail="Movie not in cart")
 
-    # notify_moderators(f"User {user.id} removed movie {cart_item.movie.name} from the cart.")
     delete_cart_item(cart_item, db)
     return CartItemResponse(message="Movie removed from cart")
 
 
 @router.delete("/clear", response_model=CartItemResponse)
 def clear_cart(
-        db: Session = Depends(get_postgresql_db), user: User = Depends(get_current_user)
+        db: Session = Depends(get_postgresql_db),
+        token: str = Depends(get_token),
+        jwt_manager=Depends(get_jwt_auth_manager)
 ):
+    try:
+        payload = jwt_manager.decode_access_token(token)
+        user_id = payload.get("user_id")
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=str(e)
+        )
+
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
     cart = get_user_cart(user, db)
     if not cart or not cart.items:
         raise HTTPException(status_code=404, detail="Cart is already empty")
@@ -93,46 +177,108 @@ def clear_cart(
     return CartItemResponse(message="Cart cleared successfully")
 
 
-@router.get("/purchased")
-def list_purchased_movies(
+@router.post("/checkout")
+def checkout(
         db: Session = Depends(get_postgresql_db),
-        user: User = Depends(get_current_user)
+        token: str = Depends(get_token),
+        jwt_manager=Depends(get_jwt_auth_manager)
 ):
-    purchased_movies = get_purchased_movies(user, db)
-
-    return {"purchased_movies": [movie.name for movie in purchased_movies]}
-
-
-@router.get("/admin/carts")
-def get_all_carts(
-        db: Session = Depends(get_postgresql_db),
-        user: User = Depends(get_current_user)
-):
-    if user.group.name != "moderator":
-        raise HTTPException(status_code=403, detail="You are not authorized")
-
-    all_carts = db.query(Cart).all()
-    return {
-        "carts": [
-            {
-                "user_id": cart.user_id,
-                "movies": [item.movie.name for item in cart.items]
-            }
-            for cart in all_carts
-        ]
-    }
-
-
-def get_cart_items_details(cart):
-    return [
-        CartItemDetail(
-            movie_id=item.movie.id,
-            title=item.movie.name,
-            price=item.movie.price,
-            genre=(
-                item.movie.genres[0].name if item.movie.genres else "Unknown"
-            ),
-            release_year=item.movie.year
+    try:
+        payload = jwt_manager.decode_access_token(token)
+        user_id = payload.get("user_id")
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=str(e)
         )
-        for item in cart.items
-    ] if cart else []
+
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
+    if not user.is_active:
+        raise HTTPException(
+            status_code=403,
+            detail="Please activate your account before making a purchase."
+        )
+
+    cart = get_user_cart(user, db)
+    if not cart or not cart.items:
+        raise HTTPException(status_code=400, detail="Your cart is empty")
+
+    order = Order(
+        user_id=user.id,
+        total_amount=sum(item.movie.price for item in cart.items)
+    )
+    db.add(order)
+    db.commit()
+    db.refresh(order)
+
+    order_items = []
+    for item in cart.items:
+        order_item = OrderItem(
+            order_id=order.id,
+            movie_id=item.movie.id,
+            price_at_order=item.movie.price
+        )
+        db.add(order_item)
+        order_items.append(order_item)
+
+    db.commit()
+
+    payment = Payment(
+        user_id=user.id,
+        order_id=order.id,
+        status=PaymentStatusEnum.PENDING,
+        amount=order.total_amount,
+        external_payment_id=None
+    )
+    db.add(payment)
+    db.commit()
+    db.refresh(payment)
+
+    for order_item in order_items:
+        payment_item = PaymentItem(
+            payment_id=payment.id,
+            order_item_id=order_item.id,
+            price_at_payment=order_item.price_at_order
+        )
+        db.add(payment_item)
+
+    db.commit()
+
+    db.query(CartItem).filter(CartItem.cart_id == cart.id).delete()
+    db.commit()
+
+    return MessageResponseSchema(message="Payment successful")
+
+
+@router.get("/purchased")
+def get_purchased_movies(
+        db: Session = Depends(get_postgresql_db),
+        token: str = Depends(get_token),
+        jwt_manager=Depends(get_jwt_auth_manager)
+):
+    try:
+        payload = jwt_manager.decode_access_token(token)
+        user_id = payload.get("user_id")
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=str(e)
+        )
+
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
+    purchased_movies = get_purchased_movies_from_db(user, db)
+    return PurchasedMoviesResponse(
+        purchased_movies=[movie.name for movie in purchased_movies]
+    )
